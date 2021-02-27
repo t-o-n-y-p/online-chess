36106bd01053cf096d277885f4e1c0445f4582e5
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2021-02-28 01:12:29 +0300

    added game list page

--- src/main/java/com/tonyp/onlinechess/web/GameListPageController.java
+++ src/main/java/com/tonyp/onlinechess/web/GameListPageController.java
@@ -1,6 +1,8 @@
 package com.tonyp.onlinechess.web;
 
+import com.tonyp.onlinechess.dao.GamesDao;
 import com.tonyp.onlinechess.dao.UsersDao;
+import com.tonyp.onlinechess.model.Game;
 import com.tonyp.onlinechess.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,43 +10,45 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Controller
-public class OpponentListPageController {
+public class GameListPageController {
 
     private final int COLUMNS = 2;
     private final int PAGE_RESULTS = 20;
-    private final int RATING_THRESHOLD = 50;
 
     private final UsersDao usersDao;
+    private final GamesDao gamesDao;
 
-    public OpponentListPageController(@Autowired UsersDao usersDao) {
+    public GameListPageController(@Autowired UsersDao usersDao, @Autowired GamesDao gamesDao) {
         this.usersDao = usersDao;
+        this.gamesDao = gamesDao;
     }
 
-    @GetMapping("/opponents")
+    @GetMapping("/games")
     public String opponents(Model model,
-                       @RequestParam(defaultValue = "1") int page,
-                       @RequestParam(defaultValue = "") String search) {
+                            @RequestParam(defaultValue = "1") int page,
+                            @RequestParam(defaultValue = "") String search) {
         model.addAttribute("search", search);
         model.addAttribute("page", page);
         model.addAttribute("columns", COLUMNS);
         User user = usersDao.findByLogin(OnlineChessApplication.USER_LOGIN);
-        List<User> opponents = usersDao.findOpponentsByRatingAndLoginInput(
-                    user, search, user.getRating(), RATING_THRESHOLD, (page - 1) * PAGE_RESULTS, PAGE_RESULTS + 1
+        model.addAttribute("user", user);
+        List<Game> games = gamesDao.findByUserAndOpponentLoginInput(
+                user, search, (page - 1) * PAGE_RESULTS, PAGE_RESULTS + 1
         );
         AtomicInteger counter = new AtomicInteger(0);
-        Map<Integer, List<User>> opponentsMap = opponents.stream()
+        Map<Integer, List<Game>> gamesMap = games.stream()
                 .limit(PAGE_RESULTS)
                 .collect(Collectors.groupingBy(i -> counter.getAndIncrement() % COLUMNS));
-        model.addAttribute("opponentsMap", opponentsMap);
-        model.addAttribute("nextPageAvailable", opponents.size() > PAGE_RESULTS);
+        model.addAttribute("gamesMap", gamesMap);
+        model.addAttribute("nextPageAvailable", games.size() > PAGE_RESULTS);
 
-        return "opponents";
+        return "games";
     }
+
 }

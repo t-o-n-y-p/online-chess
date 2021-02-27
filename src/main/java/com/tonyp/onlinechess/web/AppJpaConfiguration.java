3a82f2b4e62ec1bbbb5b0bc991cf3b3844792c48
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2021-02-27 20:04:01 +0300

    added main page

--- src/main/java/com/tonyp/onlinechess/web/AppJpaConfiguration.java
+++ src/main/java/com/tonyp/onlinechess/web/AppJpaConfiguration.java
@@ -1,6 +1,7 @@
 package com.tonyp.onlinechess.web;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
@@ -8,6 +9,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 @Configuration
+@ComponentScan(basePackages = "com.tonyp.onlinechess.dao")
 public class AppJpaConfiguration {
     @Bean
     public EntityManager entityManager(EntityManagerFactory factory) {
---
+++ src/main/java/com/tonyp/onlinechess/web/MainPageController.java
@@ -0,0 +1,53 @@
+package com.tonyp.onlinechess.web;
+
+import com.tonyp.onlinechess.dao.ChallengesDao;
+import com.tonyp.onlinechess.dao.GamesDao;
+import com.tonyp.onlinechess.dao.UsersDao;
+import com.tonyp.onlinechess.model.Challenge;
+import com.tonyp.onlinechess.model.Game;
+import com.tonyp.onlinechess.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Controller
+public class MainPageController {
+    private final int MAIN_PAGE_RESULTS = 10;
+
+    @Autowired
+    private EntityManager manager;
+
+    @Autowired
+    private UsersDao usersDao;
+
+    @Autowired
+    private ChallengesDao challengesDao;
+
+    @Autowired
+    private GamesDao gamesDao;
+
+    @GetMapping("/main")
+    public String main(Model model,
+                       @RequestParam(defaultValue = "false", name = "challenge_accepted") boolean challengeAccepted,
+                       @RequestParam(defaultValue = "false", name = "challenge_error") boolean challengeError) {
+        model.addAttribute("challengeAccepted", challengeAccepted);
+        model.addAttribute("challengeError", challengeError);
+        User user = usersDao.findByLogin("test1614438140908");
+        model.addAttribute("user", user);
+        List<Challenge> incomingChallenges = challengesDao.findIncomingChallenges(user, 0, MAIN_PAGE_RESULTS + 1);
+        model.addAttribute("incomingChallenges",
+                incomingChallenges.subList(0, Integer.min(incomingChallenges.size(), MAIN_PAGE_RESULTS)));
+        model.addAttribute("canViewAllChallenges", incomingChallenges.size() > MAIN_PAGE_RESULTS);
+        List<Game> games = gamesDao.findByUser(user, 0, MAIN_PAGE_RESULTS + 1);
+        model.addAttribute("games", games.subList(0, Integer.min(games.size(), MAIN_PAGE_RESULTS)));
+        model.addAttribute("canViewAllGames", games.size() > MAIN_PAGE_RESULTS);
+        return "main";
+    }
+
+}

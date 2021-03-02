ab2ba9b2eec4a5f551a7090f1f2732c7674398f5
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2021-03-02 23:13:29 +0300

    added stockfish engine binding check

--- src/main/java/com/tonyp/onlinechess/web/OnlineChessApplication.java
+++ src/main/java/com/tonyp/onlinechess/web/OnlineChessApplication.java
@@ -1,12 +1,17 @@
 package com.tonyp.onlinechess.web;
 
+import com.tonyp.onlinechess.tools.GameUtil;
+import com.tonyp.onlinechess.tools.StockfishUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class OnlineChessApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        StockfishUtil.makeMove(GameUtil.STARTING_POSITION_FEN, "e2e4");
         SpringApplication.run(OnlineChessApplication.class, args);
     }
 
